f71338e0e1593a188cfa3bd30b0367bda952a74f
Arun Kumar <stockerunknown@gmail.com>
2025-02-02 23:04:06 +0530

    Learn about what is Variables in Python

---
+++ Python/Variable.py
@@ -0,0 +1,15 @@
+# Variables in Python
+
+# Variables are used to store data values. A variable is a name given to a memory location in a program. Variables are created when you assign a value to it. Variables do not need to be declared with any particular type and can even change type after they have been set.
+
+# Variable names are case-sensitive.
+
+
+integer_variable = 10
+float_variable = 10.5
+string_variable = "Hello World"
+boolean_variable = True
+
+print(
+    f"This is integer = {integer_variable}, float = {float_variable}, string = '{string_variable}', boolean = {boolean_variable}"
+)
