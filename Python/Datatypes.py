fa653f6ee989d821b34d9b4a3c27906ec1ecc2c1
Arun Kumar <stockerunknown@gmail.com>
2025-02-03 22:14:27 +0530

    Data types in Python

---
+++ Python/Datatypes.py
@@ -0,0 +1,14 @@
+#  Diffrent types of data types in python
+
+# # Data Types in Python
+
+# Text Type :	str
+# Numeric Types :	int, float, complex
+# Sequence Types :	list, tuple, range
+# Mapping Type :	dict
+# Set Types :	set, frozenset
+# Boolean Type :	bool
+# Binary Types :	bytes, bytearray, memoryview
+
+# # Getting the Data Type
+
