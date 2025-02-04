9fc40428adf2ef07959585c685e7854467f62638
Arun Kumar <stockerunknown@gmail.com>
2025-02-05 00:34:16 +0530

    Learn about input take and how to take input for diffrent data types

---
+++ Python/UserInput.py
@@ -0,0 +1,19 @@
+# How to take input from user in Python?
+
+# String
+roll_number= str(input("Enter a your name: ")) 
+print("Your name is: ", roll_number)
+# Integer
+roll_number= int(input("Enter a Roll Number: "))
+print("Your Roll Number is: ", roll_number)
+# Float
+cgpa= float(input("Enter your CGPA: "))
+print("Your CGPA is: ", cgpa)
+
+
+#  Evaluate is what
+#  For Example
+exp= eval(input("Enter an expression: "))
+print(exp)
+# And user enter 2+3 then output will be 5 so this is how it takes expression
+
