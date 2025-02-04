04612fe062d7f7f135088a47efc8afcc71d15874
Arun Kumar <stockerunknown@gmail.com>
2025-02-05 01:04:09 +0530

    Doing Problem Solving Solve 5th Problem

---
+++ Python/ProbelmSolving/Program5.py
@@ -0,0 +1,9 @@
+#  WAP to take an user input as integer and then convert it into float
+
+
+length = int(input("Enter the length of the rectangle: "))
+breadth = int(input("Enter the breadth of the rectangle: "))
+
+
+area = float(length) * float(breadth)
+print(f"The area of the rectangle is: {area}")
