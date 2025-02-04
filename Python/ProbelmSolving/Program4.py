7ad1b40de32d13695685cf16e0e898c776cde012
Arun Kumar <stockerunknown@gmail.com>
2025-02-05 01:00:46 +0530

    Doing Problem Solving Solve 4th Problem

--- Python/ProbelmSolving/Program4.py
+++ Python/ProbelmSolving/Program4.py
@@ -1,20 +1,20 @@
+#  Write  a program to take details from student for ID-Card and then print it in diffrent lines
+
+name = str(input("Enter your name: "))
+age = int(input("Enter your age: "))
+rollNo = int(input("Enter your Roll No: "))
+sessionstart = int(input("Enter Date of Starting year: "))
+sessionend = int(input("Enter Date of Ending year: "))
+Class = str(input("Enter your class: "))
 
 
-name=str(input("Enter your name: "))
-age=int(input("Enter your age: "))
-rollNo=int(input("Enter your Roll No: "))
-sessionstart=int(input("Enter Date of Starting year: "))
-sessionend=int(input("Enter Date of Ending year: "))
-Class=str(input("Enter your class: "))
-
-
-
-
-print(f"""
+print(
+    f"""
 Name:{name}
 Age:{age}
 Roll No:{rollNo}
 Starting Year: {sessionstart}
 Ending Year: {sessionend}
 Class: {Class}
-      """)
+      """
+)
