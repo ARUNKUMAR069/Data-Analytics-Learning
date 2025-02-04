4ea25f08a2bf1dc18755f2bdf999492764988dc3
Arun Kumar <stockerunknown@gmail.com>
2025-02-05 01:00:36 +0530

    Doing Problem Solving Solve 4th Problem

---
+++ Python/ProbelmSolving/Program4.py
@@ -0,0 +1,20 @@
+
+
+name=str(input("Enter your name: "))
+age=int(input("Enter your age: "))
+rollNo=int(input("Enter your Roll No: "))
+sessionstart=int(input("Enter Date of Starting year: "))
+sessionend=int(input("Enter Date of Ending year: "))
+Class=str(input("Enter your class: "))
+
+
+
+
+print(f"""
+Name:{name}
+Age:{age}
+Roll No:{rollNo}
+Starting Year: {sessionstart}
+Ending Year: {sessionend}
+Class: {Class}
+      """)
