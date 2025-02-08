b8fbbc447a49f0b96c4ad7d6f5ba1d2c770c251b
Arun Kumar <stockerunknown@gmail.com>
2025-02-08 11:06:10 +0530

    Learn about Comparison Operators

--- Python/OperatorsAndOperands.py
+++ Python/OperatorsAndOperands.py
@@ -27,3 +27,38 @@ print("Modulus:",Modulus)
 print("Exponentiation:",Exponentiation)
 print("FloorDivision:",FloorDivision)
 # Output:
+
+
+
+#  Comparison Operators
+#  Types of Comparison Operators
+#  Equal
+#  Not equal
+#  Greater than
+#  Less than
+#  Greater than or equal to
+#  Less than or equal to
+
+
+# Equal
+Equal=10==5
+# Not equal
+NotEqual=10!=5
+# Greater than
+GreaterThan=10>5
+# Less than
+LessThan=10<5
+# Greater than or equal to
+GreaterThanOrEqual=10>=5
+# Less than or equal to
+LessThanOrEqual=10<=5
+print("Equal:",Equal)
+print("NotEqual:",NotEqual)
+print("GreaterThan:",GreaterThan)
+print("LessThan:",LessThan)
+print("GreaterThanOrEqual:",GreaterThanOrEqual)
+print("LessThanOrEqual:",LessThanOrEqual)
+# Output:
+
+
+
