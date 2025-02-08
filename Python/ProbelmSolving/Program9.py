47a1c2096465738f37cf274c5905162287f6f98c
Arun Kumar <stockerunknown@gmail.com>
2025-02-09 01:22:26 +0530

    WAP to check the letter is a vowel or not

---
+++ Python/ProbelmSolving/Program9.py
@@ -0,0 +1,8 @@
+#  WAJP to check whether the entered letter is a vowel or a consonant.
+letter= input("Enter a letter: ")
+letters=['a','e','i','o','u','A','E','I','O','U']
+
+if letter in letters:
+    print("The letter is a vowel")
+else:
+    print("The letter is a consonant")
