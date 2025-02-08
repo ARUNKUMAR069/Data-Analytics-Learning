10e86481b3fb6bd04c7a6c45ee9934dc06dfbb67
Arun Kumar <stockerunknown@gmail.com>
2025-02-08 11:02:42 +0530

    Learn About Arithmetic Operators

--- Python/OperatorsAndOperands.py
+++ Python/OperatorsAndOperands.py
@@ -1,2 +1,29 @@
-#  Arithmetic Operators
+#  Python  Arithmetic Operators
 
+# Types of Arithmetic Operators
+# Addition
+# Subtraction
+# Multiplication
+# Division
+# Modulus
+# Exponentiation
+# Floor division
+
+
+
+# Addition
+Addition=10+5
+Subtraction=10-5
+Multiplication=10*5
+Division=10/5
+Modulus=10%5
+Exponentiation=10**2
+FloorDivision=10//5
+print("Addition:",Addition)
+print("Subtraction:",Subtraction)
+print("Multiplication:",Multiplication)
+print("Division:",Division)
+print("Modulus:",Modulus)
+print("Exponentiation:",Exponentiation)
+print("FloorDivision:",FloorDivision)
+# Output:
