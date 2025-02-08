8f62f63ecee1852ecc62c060af7c0665d4e10c07
Arun Kumar <stockerunknown@gmail.com>
2025-02-09 00:10:06 +0530

    WAP to check whether the number is positive negative or zero

---
+++ Python/ProbelmSolving/Program6.py
@@ -0,0 +1,10 @@
+# Write a program to check whether a number is positive, negative or zero
+
+
+number=int(input("Enter a number: "))
+if number>0:
+    print("The number is positive")
+elif number==0:
+    print("The number is zero")
+else:
+    print("The number is negative")
